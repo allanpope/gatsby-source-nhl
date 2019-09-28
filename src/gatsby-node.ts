4ab547adfaa2c9db9b61116fcf6bc611bf3751e2
Allan Pope <pope.allans@gmail.com>
2019-09-28 14:00:14 +1000

    create a node with teams name

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -2,10 +2,31 @@ import { SourceNodesArgs } from "gatsby"
 
 import { getTeamData } from "./nhl-api"
 
+interface Team {
+  id: number
+  name: string
+}
+
 export const sourceNodes = async (
-  { actions, cache, createContentDigest }: SourceNodesArgs,
+  { actions, createNodeId, createContentDigest }: SourceNodesArgs,
   {}
 ) => {
+  const { createNode } = actions
+
   const data = await getTeamData()
-  console.log("data: ", data)
+
+  data.teams.map((team: Team) => {
+    const nodeId = createNodeId(`team-${team.id}`)
+    const nodeContent = JSON.stringify(team)
+
+    createNode({
+      id: nodeId,
+      name: team.name,
+      internal: {
+        type: `NHLTeam`,
+        content: nodeContent,
+        contentDigest: createContentDigest(team),
+      },
+    })
+  })
 }
