fa6be107d790ed1f1775bc1fdbe863dd45c19744
Allan Pope <pope.allans@gmail.com>
2019-11-06 21:45:18 +1300

    Split into sep files

---
+++ src/createSchemaCustomization.ts
@@ -0,0 +1,38 @@
+const createSchemaCustomization = ({ actions }: any) => {
+  const { createTypes } = actions;
+
+  createTypes(`
+    type NHLRosterItem implements Node {
+      player: NHLPlayer @link,
+      position: NHLPosition @link,
+      team: NHLTeam @link
+    }
+
+    type NHLPosition implements Node {
+      players: [NHLPlayer] @link
+    }
+
+    type NHLPlayer implements Node {
+      position: NHLPosition @link
+      team: NHLTeam @link
+    }
+
+    type NHLFranchise implements Node {
+      team: NHLTeam @link
+    }
+
+    type NHLDivision implements Node {
+      teams: [NHLTeam] @link
+    }
+
+    type NHLConference implements Node {
+      teams: [NHLTeam] @link
+    }
+
+    type NHLVenue implements Node {
+      team: NHLTeam @link
+    }
+  `);
+};
+
+export default createSchemaCustomization;
