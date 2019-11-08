7fb129401335187254433062b95b960622885f62
Allan Pope <pope.allans@gmail.com>
2019-11-08 16:24:14 +1300

    Add tests for build conference nodes

--- src/nodes/build-conference-nodes.ts
+++ src/nodes/build-conference-nodes.ts
@@ -8,45 +8,25 @@ const buildConferenceNodes = (
   teams: Team[],
   { createNodeId, createContentDigest }: NodePluginArgs,
 ) => {
-  // const teamsGroupedByConference = groupBy(teams, 'conference.name');
-  // const nodes = [];
+  const teamsGroupedByConference = groupBy(teams, 'conference.name');
+  const nodes = [];
 
-  // for (const [key, value] of Object.entries(teamsGroupedByConference)) {
-  //   nodes.push({
-  //     id: createNodeId(value[0].conference.id),
-  //     externalId: value[0].conference.id,
-  //     name: key,
-  //     slug: slugify(key, { lower: true }),
-  //     teams: value.map((team: Team) => createNodeId(team.id)),
-  //     internal: {
-  //       type: `NHLConference`,
-  //       content: JSON.stringify(value),
-  //       contentDigest: createContentDigest(value),
-  //     },
-  //   });
-  // }
+  for (const [key, value] of Object.entries(teamsGroupedByConference)) {
+    nodes.push({
+      id: createNodeId(value[0].conference.id),
+      externalId: value[0].conference.id,
+      name: key,
+      slug: slugify(key, { lower: true }),
+      teams: value.map((team: Team) => createNodeId(team.id)),
+      internal: {
+        type: `NHLConference`,
+        content: JSON.stringify(value),
+        contentDigest: createContentDigest(value),
+      },
+    });
+  }
 
-  // return nodes;
-
-  const teamsByConferences = groupBy(
-    teams,
-    (team: Team) => team.conference.name,
-  );
-
-  return teams.map((team: Team) => ({
-    id: createNodeId(team.conference.id),
-    externalId: team.conference.id,
-    name: team.conference.name,
-    slug: slugify(team.conference.name, { lower: true }),
-    teams: teamsByConferences[team.conference.name].map((team: Team) =>
-      createNodeId(team.id),
-    ),
-    internal: {
-      type: `NHLConference`,
-      content: JSON.stringify(team.conference),
-      contentDigest: createContentDigest(team.conference),
-    },
-  }));
+  return nodes;
 };
 
 export default buildConferenceNodes;
--- tests/nodes/build-conference-nodes.test.ts
+++ tests/nodes/build-conference-nodes.test.ts
@@ -2,15 +2,68 @@ import buildConferenceNodes from '../../src/nodes/build-conference-nodes';
 import teams from '../fixtures/teams';
 
 describe('buildConferenceNodes', () => {
+  let result: any;
   const createNodeHelpers: any = {
     createNodeId: jest.fn(() => 'mock-id'),
     createContentDigest: jest.fn(() => 'mock-digest'),
   };
 
-  it('returns a object for each conference', () => {
-    console.log(buildConferenceNodes(teams, createNodeHelpers));
-    expect(buildConferenceNodes(teams, createNodeHelpers).length).toHaveLength(
-      2,
+  beforeEach(() => {
+    jest.clearAllMocks();
+    result = buildConferenceNodes(teams, createNodeHelpers);
+  });
+
+  it('returns two objects representing each conference', () => {
+    expect(result).toHaveLength(2);
+  });
+
+  it('calls createContentDigest for each conference', () => {
+    expect(createNodeHelpers.createContentDigest).toHaveBeenCalledTimes(2);
+  });
+
+  it('calls createNodeId with the conference id', () => {
+    expect(createNodeHelpers.createNodeId).toHaveBeenCalledWith(
+      teams[0].conference.id,
     );
+  });
+
+  it('returns a object with the required gatsby fields', () => {
+    expect(result[0]).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        internal: {
+          type: expect.any(String),
+          content: expect.anything(),
+          contentDigest: expect.anything(),
+        },
+      }),
+    );
+  });
+
+  it('returns a object with conference fields', () => {
+    expect(result[0]).toEqual(
+      expect.objectContaining({
+        externalId: expect.any(Number),
+        name: expect.any(String),
+        slug: expect.any(String),
+        teams: expect.any(Array),
+      }),
+    );
+  });
+
+  it('creates a slug for each conference', () => {
+    expect(result).toEqual([
+      expect.objectContaining({
+        slug: 'eastern',
+      }),
+      expect.objectContaining({
+        slug: 'western',
+      }),
+    ]);
+  });
+
+  it('creates a an array of ids for each team in a conference', () => {
+    expect(result[0].teams).toHaveLength(16);
+    expect(result[1].teams).toHaveLength(15);
   });
 });
